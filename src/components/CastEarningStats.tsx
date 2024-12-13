f7b01115edfeea82f921b0e5cbb88616bbc18bec
Nikolai <nikolai1973@gmail.com>
2024-12-13 12:35:10 +0700

    added threshold for loading

--- src/components/CastEarningStats.tsx
+++ src/components/CastEarningStats.tsx
@@ -217,7 +217,7 @@ export default function CastEarningStats({ title = "Cast Earning Stats by @nikol
         }
       },
       { 
-        rootMargin: '200px', // Load when element is 100px below viewport
+        rootMargin: '300px', // Load when element is 100px below viewport
         threshold: 0
       }
     );
